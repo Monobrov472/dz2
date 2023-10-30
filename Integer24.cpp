49ba5ba2f6b3813deeb04be6392a516d0e80b644
Monobrov472 <144647756+Monobrov472@users.noreply.github.com>
2023-10-30 10:26:25 +0200

    Add files via upload

---
+++ Integer24.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int K;
+
+    // Введення значення K
+    cout << "Enter value K (from 1 to 365): ";
+    cin >> K;
+
+    // Понеділок відповідає 1, тому ми віднімаємо 1 від K
+    K = K - 1;
+
+    // Визначення номера дня тижня
+    int day_of_week = K % 7;
+
+    // Виведення результату
+    cout << "Day number " << K + 1 << "-th day of the year: " << day_of_week << endl;
+
+    return 0;
+}
